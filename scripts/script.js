27093ab4d8d40146057ec66502a6bcca86577d67
YanMennella <yancorp.tecdev@gmail.com>
2025-07-26 13:55:40 -0300

    Função > Esconder / mostrar barra lateral

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const sidebar = document.getElementById('sidebar');
+const closeBtn = document.getElementById('close-btn');
+
+let isSidebarVisible = true;
+
+closeBtn.addEventListener('click', () => {
+    if (isSidebarVisible) {
+        sidebar.style.left = '-380px';
+        isSidebarVisible = false;
+    } else {
+        sidebar.style.left = '0';
+        isSidebarVisible = 'true';
+    }    
+});
