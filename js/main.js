86907c0943f0fc0cb73150dc349abd0b644e695d
YanMennella <yancorp.tecdev@gmail.com>
2025-08-23 13:43:00 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Esconde e mostra a sidebar
 const sidebar = document.getElementById('sidebar');
 const closeBtn = document.getElementById('close-btn');
 
@@ -11,4 +12,31 @@ closeBtn.addEventListener('click', () => {
         sidebar.style.left = '0';
         isSidebarVisible = true;
     }    
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdownButtons = document.querySelectorAll(".dropbtn");
+
+    dropdownButtons.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+          // Fecha todos os outros abertos antes de abrir o clicado
+          document.querySelectorAll(".dropdown-content").forEach((menu) => {
+            if (menu !== btn.nextElementSibling) {
+                menu.classList.remove("show");
+            }
+          });
+
+          // Alterna o submenu do botão clicado
+          btn.nextElementSibling.classList.toggle("show");
+        });
+    });
+
+    // Fecha o menu se clicar fora
+    window.addEventListener("click", (e) => {
+        if (!e.target.matches(".dropbtn")) {
+            document.querySelectorAll(".dropdown-content").forEach((menu) => {
+                menu.classList.remove("show");
+            });
+        }
+    });
 });
